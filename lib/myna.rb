cac7755cd57e947074772d727e9ebdc595d9a039
Noel Welsh <noel@untyped.com>
2012-05-11 10:36:58 +0100

    Implement the API for the authorized actions. Not tested yet.

--- lib/myna.rb
+++ lib/myna.rb
@@ -54,21 +54,68 @@ module Myna
     EventMachine::stop_event_loop
   end
 
-  module API
+  module AuthorizedHost
     SCHEME = "https"
     HOST   = 'api.mynaweb.com'
     PORT   = 443
   end
 
-  class Path
-    include Singleton
+  module UnauthorizedHost
+    SCHEME = "http"
+    HOST   = 'api.mynaweb.com'
+    PORT   = 80
+  end
+
+  class Api
+    def initialize(api)
+      @api = api
+    end
+
+    def make_request(uri)
+      puts "Contacting "+uri
+      future = Future.new
+
+      EventMachine::run do
+        client = EventMachine::HttpRequest.new(uri)
+
+        http = yield(client)
+        http.errback  { future.deliver("badness") }
+        http.callback { future.deliver(Response.parse(http.response)) }
+      end
+
+      future
+    end
+
+    def build_uri(path)
+      "#{@api::SCHEME}://#{@api::HOST}:#{@api::PORT}#{path}"
+    end
+
+    def create
+      build_uri("/v1/experiment/new")
+    end
 
     def suggest(uuid)
-      "/v1/experiment/#{uuid}/suggest"
+      build_uri("/v1/experiment/#{uuid}/suggest")
     end
 
     def reward(uuid)
-      "/v1/experiment/#{uuid}/reward"
+      build_uri("/v1/experiment/#{uuid}/reward")
+    end
+
+    def delete(uuid)
+      build_uri("/v1/experiment/#{uuid}/delete")
+    end
+
+    def info(uuid)
+      build_uri("/v1/experiment/#{uuid}/info")
+    end
+
+    def create_variant(uuid)
+      build_uri("/v1/experiment/#{uuid}/new-variant")
+    end
+
+    def delete_variant(uuid)
+      build_uri("/v1/experiment/#{uuid}/delete-variant")
     end
   end
 
@@ -99,45 +146,98 @@ module Myna
     end
   end
 
+  # The simple API for unauthorized access
   class Experiment
-    def initialize(uuid, scheme = API::SCHEME, host = API::HOST, port = API::PORT, path = Path.instance)
-      @uuid   = uuid
-      @host   = host
-      @port   = port
-      @scheme = scheme
-      @path   = path
+    def initialize(uuid, host = UnauthorizedHost)
+      @uuid = uuid
+      @host = host
+      @api  = Api.new(host)
     end
 
-    def build_uri(path)
-      "#{@scheme}://#{@host}:#{@port}#{path}"
-    end
-
-    def make_request(path)
-      uri = build_uri(path)
-      puts "Contacting "+uri
-      future = Future.new
-
-      EventMachine::run do
-        client = EventMachine::HttpRequest.new(uri)
-
-        http = yield(client)
-        http.errback  { future.deliver("badness") }
-        http.callback { future.deliver(Response.parse(http.response)) }
-      end
-
-      future
-    end
+    attr_reader :uuid
 
     def suggest()
-      make_request(@path.suggest(@uuid)) do |client|
+      @api.make_request(@api.suggest(@uuid)) do |client|
         client.get(:head => {'Accept' => 'application/json'})
       end
     end
 
     def reward(token, amount = 1.0)
-      make_request(@path.reward(@uuid)) do |client|
+      @api.make_request(@api.reward(@uuid)) do |client|
         client.post(:head => {'Accept' => 'application/json'},
                     :body => {:token => token, :amount => amount}.to_json)
+      end
+    end
+  end
+
+  # The more complex API for authorized access
+  class ExperimentFactory
+    def initialize(email, password, host = AuthorizedHost)
+      @email    = email
+      @password = password
+      @host     = host
+      @api      = Api.new(host)
+    end
+
+    def create(name)
+      @api.make_request(@api.create()) do |client|
+        client.post(:head => {
+                      'Accept' => 'application/json',
+                      'Authorization' => [@email, @password]
+                    },
+                    :body => {:experiment => name}.to_json)
+
+      end
+    end
+
+    def experiment(uuid)
+      AuthorizedExperiment.new(uuid, @email, @password, @host)
+    end
+  end
+
+  class AuthorizedExperiment < Experiment
+    def initialize(uuid, email, password, host)
+      super(uuid, host)
+      @email    = email
+      @password = password
+    end
+
+    def delete
+      @api.make_request(@api.delete(@uuid)) do |client|
+        client.get(:head => {
+                     'Accept' => 'application/json',
+                     'Authorization' => [@email, @password]
+                   })
+      end
+    end
+
+    def info
+      @api.make_request(@api.info(@uuid)) do |client|
+        client.get(:head => {
+                     'Accept' => 'application/json',
+                     'Authorization' => [@email, @password]
+                   })
+      end
+    end
+
+    def create_variant(name)
+      @api.make_request(@api.new_variant()) do |client|
+        client.post(:head => {
+                      'Accept' => 'application/json',
+                      'Authorization' => [@email, @password]
+                    },
+                    :body => {:variant => name}.to_json)
+
+      end
+    end
+
+    def delete_variant(name)
+      @api.make_request(@api.delete_variant()) do |client|
+        client.post(:head => {
+                      'Accept' => 'application/json',
+                      'Authorization' => [@email, @password]
+                    },
+                    :body => {:variant => name}.to_json)
       end
     end
   end
